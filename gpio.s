20ecbe4fb5f43b2cfa4099abd5c7b6a3879f2730
Fernando Abreu <fabreu.real@gmail.com>
2024-05-06 02:08:54 -0300

    v1.0 - Criação de subrotinas para controlar LCD.

---
+++ gpio.s
@@ -0,0 +1,454 @@
+; gpio.s
+; Desenvolvido para a placa EK-TM4C1294XL
+; Autor: Fernando Pereira Gomes de Abreu
+; 05/05/2024
+
+; -------------------------------------------------------------------------------
+        THUMB                        ; Instruções do tipo Thumb-2
+; -------------------------------------------------------------------------------
+; ========================
+; Definições dos Registradores Gerais
+SYSCTL_RCGCGPIO_R	 EQU	0x400FE608
+SYSCTL_PRGPIO_R		 EQU    0x400FEA08
+; Definições dos Ports
+;
+; PORT J
+GPIO_PORTJ_AHB_LOCK_R    	EQU    	0x40060520
+GPIO_PORTJ_AHB_CR_R      	EQU    	0x40060524
+GPIO_PORTJ_AHB_AMSEL_R   	EQU   	0x40060528
+GPIO_PORTJ_AHB_PCTL_R    	EQU   	0x4006052C
+GPIO_PORTJ_AHB_DIR_R     	EQU    	0x40060400
+GPIO_PORTJ_AHB_AFSEL_R   	EQU    	0x40060420
+GPIO_PORTJ_AHB_DEN_R     	EQU    	0x4006051C
+GPIO_PORTJ_AHB_PUR_R     	EQU    	0x40060510	
+GPIO_PORTJ_AHB_DATA_R    	EQU    	0x400603FC
+GPIO_PORTJ_AHB_DATA_BITS_R  EQU    	0x40060000
+GPIO_PORTJ_AHB_IS_R     	EQU	   	0x40060404			;	MACROS DE ENDEREÇOS DE INTERRUPÇÕES	
+GPIO_PORTJ_AHB_IBE_R    	EQU    	0x40060408
+GPIO_PORTJ_AHB_IEV_R    	EQU    	0x4006040C
+GPIO_PORTJ_AHB_IM_R     	EQU    	0x40060410
+GPIO_PORTJ_AHB_RIS_R    	EQU    	0x40060414
+GPIO_PORTJ_AHB_MIS_R    	EQU    	0x40060418
+GPIO_PORTJ_AHB_ICR_R    	EQU	   	0x4006041C
+GPIO_PORTJ               	EQU    	2_000000100000000
+;									  QPNMLKJHGFEDCBA
+; PORT K
+GPIO_PORTK_DATA_R       	EQU 	0x400613FC
+GPIO_PORTK_DIR_R        	EQU 	0x40061400
+GPIO_PORTK_IS_R         	EQU		0x40061404
+GPIO_PORTK_IBE_R        	EQU 	0x40061408
+GPIO_PORTK_IEV_R        	EQU 	0x4006140C
+GPIO_PORTK_IM_R         	EQU 	0x40061410
+GPIO_PORTK_RIS_R        	EQU 	0x40061414
+GPIO_PORTK_MIS_R        	EQU 	0x40061418
+GPIO_PORTK_ICR_R        	EQU 	0x4006141C
+GPIO_PORTK_AFSEL_R      	EQU 	0x40061420
+GPIO_PORTK_PUR_R        	EQU 	0x40061510
+GPIO_PORTK_DEN_R        	EQU 	0x4006151C
+GPIO_PORTK_LOCK_R       	EQU 	0x40061520
+GPIO_PORTK_CR_R         	EQU 	0x40061524
+GPIO_PORTK_AMSEL_R      	EQU 	0x40061528
+GPIO_PORTK_PCTL_R       	EQU 	0x4006152C
+GPIO_PORTK              	EQU		2_000001000000000
+;							  		  QPNMLKJHGFEDCBA
+; PORT M
+GPIO_PORTM_DATA_R       	EQU 	0x400633FC
+GPIO_PORTM_DIR_R       		EQU 	0x40063400
+GPIO_PORTM_IS_R         	EQU 	0x40063404
+GPIO_PORTM_IBE_R	        EQU 	0x40063408
+GPIO_PORTM_IEV_R    	    EQU 	0x4006340C
+GPIO_PORTM_IM_R         	EQU 	0x40063410
+GPIO_PORTM_RIS_R        	EQU 	0x40063414
+GPIO_PORTM_MIS_R       		EQU 	0x40063418
+GPIO_PORTM_ICR_R        	EQU 	0x4006341C
+GPIO_PORTM_AFSEL_R      	EQU 	0x40063420
+GPIO_PORTM_PUR_R        	EQU 	0x40063510
+GPIO_PORTM_DEN_R        	EQU 	0x4006351C
+GPIO_PORTM_LOCK_R       	EQU 	0x40063520
+GPIO_PORTM_CR_R         	EQU 	0x40063524
+GPIO_PORTM_AMSEL_R      	EQU 	0x40063528
+GPIO_PORTM_PCTL_R       	EQU 	0x4006352C
+GPIO_PORTM               	EQU 	2_000100000000000
+;									  QPNMLKJHGFEDCBA
+; PORT N
+GPIO_PORTN_LOCK_R    		EQU    	0x40064520
+GPIO_PORTN_CR_R      		EQU    	0x40064524
+GPIO_PORTN_AMSEL_R   		EQU    	0x40064528
+GPIO_PORTN_PCTL_R    		EQU    	0x4006452C
+GPIO_PORTN_DIR_R     		EQU    	0x40064400
+GPIO_PORTN_AFSEL_R   		EQU    	0x40064420
+GPIO_PORTN_DEN_R     		EQU    	0x4006451C
+GPIO_PORTN_PUR_R     		EQU    	0x40064510	
+GPIO_PORTN_DATA_R    		EQU    	0x400643FC
+GPIO_PORTN               	EQU    	2_001000000000000
+;									  QPNMLKJHGFEDCBA
+; -------------------------------------------------------------------------------
+; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
+;                  código
+        AREA    |.text|, CODE, READONLY, ALIGN=2
+;
+;		EXPORT E IMPORT as FUNÇÕES AQUI
+		EXPORT  GPIO_Init
+		EXPORT	LCD_Init
+		EXPORT	LCD_Write_Character
+		EXPORT	LCD_Write_String
+		EXPORT	LCD_Move_Cursor
+		EXPORT	LCD_Reset
+; -------------------------------------------------------------------------------
+		IMPORT	SysTick_Wait1ms
+		IMPORT	SysTick_Wait1us
+
+; 1. Ativar o clock para a porta setando o bit correspondente no registrador RCGCGPIO,
+; após isso verificar no PRGPIO se a porta está pronta para uso.
+; enable clock to GPIOF at clock gating register
+GPIO_Init
+            LDR     R0, =SYSCTL_RCGCGPIO_R  		;Carrega o endereço do registrador RCGCGPIO
+			MOV		R1, #GPIO_PORTK                 ;Seta o bit da porta K
+			ORR		R1, #GPIO_PORTJ					;Seta o bit da porta J, fazendo com OR
+			ORR     R1, #GPIO_PORTM					;Seta o bit da porta M, fazendo com OR
+			ORR     R1, #GPIO_PORTN					;Seta o bit da porta N, fazendo com OR
+            STR     R1, [R0]						;Move para a memória os bits das portas no endereço do RCGCGPIO
+ 
+            LDR     R0, =SYSCTL_PRGPIO_R			;Carrega o endereço do PRGPIO para esperar os GPIO ficarem prontos
+EsperaGPIO  LDR     R1, [R0]						;Lê da memória o conteúdo do endereço do registrador
+			MOV     R2, #GPIO_PORTK                 ;Seta os bits correspondentes às portas para fazer a comparação
+			ORR     R2, #GPIO_PORTJ                 ;Seta o bit da porta J, fazendo com OR
+			ORR     R1, #GPIO_PORTM					;Seta o bit da porta M, fazendo com OR
+			ORR     R1, #GPIO_PORTN					;Seta o bit da porta N, fazendo com OR
+            TST     R1, R2							;Testa o R1 com R2 fazendo R1 & R2
+            BEQ     EsperaGPIO					    ;Se o flag Z=1, volta para o laço. Senão continua executando
+; 2. Limpar o AMSEL para desabilitar a função analógica
+			MOV		R1, #0x00;
+			LDR		R0, =GPIO_PORTJ_AHB_AMSEL_R;
+			STR		R1, [R0];
+			LDR		R0, =GPIO_PORTK_AMSEL_R;
+			STR		R1, [R0];
+			LDR		R0, =GPIO_PORTM_AMSEL_R;
+			STR		R1, [R0];
+			LDR		R0, =GPIO_PORTN_AMSEL_R;
+			STR		R1, [R0];
+; 3. Limpar PCTL para selecionar o GPIO
+            MOV     R1, #0x00					    ;Colocar 0 no registrador para selecionar o modo GPIO
+			
+            LDR     R0, =GPIO_PORTJ_AHB_PCTL_R		;Carrega o R0 com o endereço do PCTL para a porta J
+            STR     R1, [R0]                        ;Guarda no registrador PCTL da porta J da memória
+			
+			LDR     R0, =GPIO_PORTK_PCTL_R			;Carrega o R0 com o endereço do PCTL para a porta K
+            STR     R1, [R0]                        ;Guarda no registrador PCTL da porta K da memória
+			
+			LDR     R0, =GPIO_PORTM_PCTL_R			;Carrega o R0 com o endereço do PCTL para a porta M
+            STR     R1, [R0]                        ;Guarda no registrador PCTL da porta M da memória
+			
+            LDR     R0, =GPIO_PORTN_PCTL_R      	;Carrega o R0 com o endereço do PCTL para a porta N
+            STR     R1, [R0]                        ;Guarda no registrador PCTL da porta N da memória
+; 4. DIR para 0 se for entrada, 1 se for saída
+			LDR		R0, =GPIO_PORTJ_AHB_DIR_R
+			MOV		R1, #2_11						;J1~J0
+			STRB	R1, [R0]
+			
+			LDR		R0, =GPIO_PORTK_DIR_R
+			MOV		R1, #2_11111111;				;K7~K0 USADO NO LCD
+			STRB	R1, [R0]
+			
+			LDR		R0, =GPIO_PORTM_DIR_R
+			MOV		R1, #2_11110111					;M7~M4 USADO NO TECLADO ; M2~M0 USADO NO LCD 
+			STRB	R1, [R0]
+			
+            LDR     R0, =GPIO_PORTN_DIR_R			;Carrega o R0 com o endereço do DIR para a porta N
+			MOV     R1, #2_0011						;PN1 PN0
+            STRB    R1, [R0]						;Guarda no registrador
+			; O certo era verificar os outros bits da PJ para não transformar entradas em saídas desnecessárias
+; 5. Limpar os bits AFSEL para 0 para selecionar GPIO 
+;    Sem função alternativa					;Escreve na porta
+			MOV     R1, #0x00						;Colocar o valor 0 para não setar função alternativa
+            LDR     R0, =GPIO_PORTJ_AHB_AFSEL_R     ;Carrega o endereço do AFSEL da porta J
+            STR     R1, [R0]                        ;Escreve na porta
+			
+			LDR     R0, =GPIO_PORTK_AFSEL_R			;Carrega o endereço do AFSEL da porta K
+            STR     R1, [R0]						;Escreve na porta
+			
+			LDR     R0, =GPIO_PORTM_AFSEL_R			;Carrega o endereço do AFSEL da porta M
+            STR     R1, [R0]						;Escreve na porta
+			
+            LDR     R0, =GPIO_PORTN_AFSEL_R			;Carrega o endereço do AFSEL da porta N
+            STR     R1, [R0]						;Escreve na porta
+; 6. Setar os bits de DEN para habilitar I/O digital 
+            LDR     R0, =GPIO_PORTJ_AHB_DEN_R		;Carrega o endereço do DEN
+			MOV     R1, #2_00000011                 ;J1 J0     
+            STRB    R1, [R0]
+			
+			LDR		R0, =GPIO_PORTK_DEN_R			;Carrega o endereço do DEN
+			MOV		R1,	#2_11111111					;K7~K0
+			STRB	R1,[R0]
+			
+			LDR		R0, =GPIO_PORTM_DEN_R			;Carrega o endereço do DEN
+			MOV		R1,	#2_11110111					;M7~M4 ; M2~M0
+			STRB	R1,[R0]
+			
+			LDR     R0, =GPIO_PORTN_DEN_R			;Carrega o endereço do DEN
+            MOV     R1, #2_00000011                 ;N1 N0
+            STRB    R1, [R0]						;Escreve no registrador da memória funcionalidade digital
+; 7. Para habilitar resistor de pull-up interno, setar PUR para 1
+			LDR     R0, =GPIO_PORTJ_AHB_PUR_R			;Carrega o endereço do PUR para a porta J
+			MOV     R1, #2_11							;Habilitar funcionalidade digital de resistor de pull-up 
+            STRB    R1, [R0]							;Escreve no registrador da memória do resistor de pull-up
+; 8. Desabilitar as configurações dos registradores GPIOIM
+			LDR		R0, =GPIO_PORTJ_AHB_IM_R
+			MOV		R1, #2_00;
+			STRB	R1, [R0];
+; 9. Configurar o tipo de interrupção no registrador GPIOIS
+;	0 -> borda
+;	1 -> nivel
+			LDR		R0, =GPIO_PORTJ_AHB_IS_R;
+			MOV		R1, #2_00;
+			STRB	R1, [R0];
+; 10. Configurar se uma ou ambas as bordas no registrador GPIOIBE
+;	0 -> uma borda
+;	1 -> ambas as bordas
+			LDR		R0, =GPIO_PORTJ_AHB_IBE_R;
+			MOV		R1, #2_00;
+			STRB	R1, [R0];
+; 11. Configurar o tipo de borda ou nivel se é subida/alta ou descida/baixo no registrador GPIOIEV
+;	0	->	borda de descida ou nível lógico baixo
+;	1	->	borda de subida ou nível lógico alto
+			LDR		R0, =GPIO_PORTJ_AHB_IEV_R;
+			MOV		R1, #2_00;
+			STRB	R1, [R0];
+; 12. Habilitar as configurações dos registradores GPIOIM
+			LDR		R0, =GPIO_PORTJ_AHB_IM_R
+			MOV		R1, #2_11;
+			STRB	R1, [R0];
+			
+; 13. Setar a prioriedade no NVIC(pág 116)  PRIx // Para a Port J o número de interrupção é 51 / deve ser x =12 
+			
+; 14. Habilitar a interrupção no NVIC(pág)  ENx  // x varia de 0 a 3
+			BX      LR;						retorno GPIO_Init
+; -------------------------------------------------------------------------------
+; -------------------------------Funções-----------------------------------------
+; -------------------------------------------------------------------------------
+;------------LCD_Init------------
+; Função de inicialização do LCD
+; Entrada: Não tem
+; Saída: Não tem
+; Modifica: R0, R1, R2, R3
+
+LCD_Init
+			LDR		R2, =GPIO_PORTK_DATA_R
+			LDR		R3,	=GPIO_PORTM_DATA_R
+			LDRB	R1, [R3];					
+			AND		R1, R1, #2_11111100;			Zero os bits M0			TO REDO
+			ORR		R1, R1, #2_00000100;			Set o bit M2~M1
+			STRB	R1, [R3];
+			
+			; 1. Escrevendo o Comando 0x38 -> Configura o LCD em modo de 8 bits e 2 linhas
+			MOV 	R1, #0x38;					
+			STRB	R1, [R2];
+			; 2. Habilitar os pinos EN R/W RS (M2~M0)
+			PUSH	{LR}
+			BL		LCD_Inst_Escr_Habi
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #40;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			
+			; 1. Escrevendo o Comando 0x06 -> autoincremento para direita
+			MOV 	R1, #0x06;					
+			STRB	R1, [R2];
+			; 2. Habilitar os pinos EN R/W RS (M2~M0)
+			PUSH	{LR}
+			BL		LCD_Inst_Escr_Habi
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #40;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			
+			; 1. Escrevendo o Comando 0x0E -> habilitar o display + cursor + não-pisca
+			MOV 	R1, #0x0F;					
+			STRB	R1, [R2];
+			; 2. Habilitar os pinos EN R/W RS (M2~M0)
+			PUSH	{LR}
+			BL		LCD_Inst_Escr_Habi
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #40;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			
+			; 1. Escrevendo o Comando 0x01 -> Reset
+			MOV		R1, #0x01;
+			STRB	R1, [R2];
+			; 2. Habilitar os pinos EN R/W RS (M2~M0)
+			PUSH	{LR}
+			BL		LCD_Inst_Escr_Habi
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #1640;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			
+			; 1. Escrevendo o Comando 0x02 -> Home
+			MOV		R1, #0x02;
+			STRB	R1, [R2];
+			; 2. Habilitar os pinos EN R/W RS (M2~M0)
+			PUSH	{LR}
+			BL		LCD_Inst_Escr_Habi
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #1640;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			
+			BX		LR;							Retorno
+;---------------------------------------------------------------
+;------------LCD_Inst_Escr_Habi------------
+; Função de habilitar os Pinos de Controles EN, R/W, RS == (100)
+; Entrada: R3(GPIO_PORTM_DATA_R)
+; Saída: Não tem
+; Modifica: R0, R1
+LCD_Inst_Escr_Habi
+			LDRB	R1, [R3];					
+			AND		R1, R1, #2_11111100;			Zero os bits M1~M0			TO REDO
+			ORR		R1, R1, #2_00000100;			Set o bit M2
+			STRB	R1, [R3];
+			MOV		R0,	#10;						Espera 10us
+			PUSH	{LR}
+			BL		SysTick_Wait1us
+			POP		{LR}
+			
+			BX		LR;		
+;---------------------------------------------------------------
+;------------LCD_DESABLE_TIME_COMAND------------
+; Função de desabilitar o EN do LCD e espera o tempo do comando
+; Entrada: R0(Tempo de espera em us), R3(GPIO_PORTM_DATA_R)
+; Saída: Não tem
+; Modifica: R0, R1
+LCD_DESABLE_TIME_COMAND
+			LDRB	R1, [R3];					
+			AND		R1, R1, #2_11111011;			Zerando os bit M2(ENABLE)			TO REDO
+			STRB	R1, [R3]
+			PUSH	{LR}
+			BL		SysTick_Wait1us
+			POP		{LR}
+			
+			BX		LR
+
+;---------------------------------------------------------------
+;-------------LCD_Write_String-------------------------------
+; Função para escrever uma string no LCD
+; Entrada: R0(Endereço da string)
+; Saída: Não tem
+; Modifica: R1
+LCD_Write_String
+			PUSH	{R0, R1, R2}
+			MOV		R2, R0
+			MOV		R1, #0;						variavel para percorrer string
+Percorrer_String
+			LDRB	R0, [R1,R2];
+			PUSH	{LR}
+			BL		LCD_Write_Character;		escrever o character
+			POP		{LR}
+			ADD		R1,R1,#1;					R1++				
+			CMP		R0, #0
+			BNE		Percorrer_String
+			POP		{R0, R1, R2}
+			BX		LR
+;---------------------------------------------------------------
+;-------------LCD_Write_Character-------------------------------
+; Função para escrever uma string no LCD
+; Entrada: R0(o caracter)
+; Saída: Não tem
+; Modifica: Nada
+LCD_Write_Character
+			PUSH	{R0,R1,R2,R3}
+			LDR		R2, =GPIO_PORTK_DATA_R
+			LDR		R3,	=GPIO_PORTM_DATA_R
+			; 1. Escrevendo o Dado(character)
+			STRB	R0, [R2];
+			; 2. Configurar os Pinos EN R/W RS para 101
+			PUSH	{LR}
+			BL		LCD_Setup_Write
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #40;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			POP		{R0,R1,R2,R3}
+			BX		LR
+;---------------------------------------------------------------
+;------------LCD_Setup_Write------------
+; Função para configurar a escrita EN R/W RS == (101)
+; Entrada: R3(GPIO_PORTM_DATA_R)
+; Saída: Não tem
+; Modifica: R0, R1
+LCD_Setup_Write
+			LDRB	R1, [R3];					
+			AND		R1, R1, #2_11111101;			Zero os bit M1			TO REDO
+			ORR		R1, R1, #2_00000101;			Set o bit M2 e M0
+			STRB	R1, [R3];
+			MOV		R0,	#10;						Espera 10us
+			PUSH	{LR}
+			BL		SysTick_Wait1us
+			POP		{LR}
+			BX		LR
+
+;---------------------------------------------------------------
+;------------------LCD_Move_Cursor------------------------------
+; Função para levar o curso do LCD para o Inicio
+; Entrada: R0
+; Saída: Não tem
+; Modifica: R0
+LCD_Move_Cursor
+			PUSH	{R0,R1,R2,R3}
+			LDR		R2, =GPIO_PORTK_DATA_R
+			LDR		R3,	=GPIO_PORTM_DATA_R
+			; 1. Escrever o comando para levar o Cursor para o inicio
+			MOV		R1, R0;
+			STRB	R1, [R2];
+			; 2. Configurar	os pinos EN R/W RS	== (100)
+			PUSH	{LR}
+			BL		LCD_Inst_Escr_Habi
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #40;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			
+			POP		{R0,R1,R2,R3}
+			BX		LR
+			
+;---------------------------------------------------------------
+;------------------LCD_Reset------------------------------
+; Função para resetar o LCD
+; Entrada: Não tem
+; Saída: Não tem
+; Modifica: Nada
+LCD_Reset
+			PUSH	{R0,R1,R2,R3}
+			LDR		R2, =GPIO_PORTK_DATA_R
+			LDR		R3,	=GPIO_PORTM_DATA_R
+			; 1. Escrevendo o Comando 0x01 -> Resetar
+			MOV 	R1, #0x01;					
+			STRB	R1, [R2];
+			; 2. Habilitar os pinos EN R/W RS (M2~M0)
+			PUSH	{LR}
+			BL		LCD_Inst_Escr_Habi
+			POP		{LR}
+			; 3. Desabilitar EN pelo tempo necessário do comando
+			PUSH	{LR}
+			MOV		R0, #1640;
+			BL		LCD_DESABLE_TIME_COMAND
+			POP		{LR}
+			POP		{R0,R1,R2,R3};
+			BX		LR
+
+	ALIGN
+	END
---
+++ main.s
@@ -0,0 +1,77 @@
+; -------------------------------------------------------------------------------
+        THUMB                        ; Instruções do tipo Thumb-2
+; -------------------------------------------------------------------------------
+		
+; Declarações EQU - Defines
+;<NOME>         EQU <VALOR>
+; ========================
+DISPLAY_U_EN	EQU	2_00100000
+DISPLAY_D_EN	EQU	2_00010000
+
+; -------------------------------------------------------------------------------
+; Área de Dados - Declarações de variáveis
+		AREA  DATA, ALIGN=2
+		; Se alguma variável for chamada em outro arquivo
+		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variável <var> a 
+		                                   ; partir de outro arquivo
+;<var>	SPACE <tam>                        ; Declara uma variável de nome <var>
+                                           ; de <tam> bytes a partir da primeira 
+                                           ; posição da RAM
+; -------------------------------------------------------------------------------
+; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
+;                  código
+        AREA    |.text|, CODE, READONLY, ALIGN=2
+		
+; constante de strings		
+MSG_COFRE_ABERTO		DCB			"  Cofre aberto  ",0 ; essa string tem 16 caracteres
+MSG_NOVA_SENHA			DCB			"  Nova senha:   ",0
+
+
+		; Se alguma função do arquivo for chamada em outro arquivo	
+        EXPORT Start                ; Permite chamar a função Start a partir de 
+			                        ; outro arquivo. No caso startup.s
+									
+		; Se chamar alguma função externa	
+        ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
+									; função <func>
+		IMPORT  PLL_Init
+		IMPORT  SysTick_Init
+		IMPORT  SysTick_Wait1ms			
+		IMPORT  GPIO_Init
+		IMPORT	LCD_Init
+		IMPORT	LCD_Write_Character
+		IMPORT	LCD_Write_String
+		IMPORT	LCD_Move_Cursor
+		IMPORT	LCD_Reset
+
+; -------------------------------------------------------------------------------
+; Função main()
+Start  		
+	BL 		PLL_Init                  	;Chama a subrotina para alterar o clock do microcontrolador para 80MHz
+	BL 		SysTick_Init
+	BL 		GPIO_Init                 	;Chama a subrotina que inicializa os GPIO
+	BL		LCD_Init					;Chama a subrotina que inicializa o LCD
+	LDR		R0, =MSG_COFRE_ABERTO		;Ponteiro para a mensagem "  Cofre aberto  " // linha 26 encontra a constante string
+	BL		LCD_Write_String			;Função de escrever uma string no LCD, presta atenção no cursor q deixei ele piscando)
+	MOV		R0, #0xC0					;Posição do inicio da segunda linha do cursor 
+	BL		LCD_Move_Cursor				;Move o Cursor na posição R0
+	LDR		R0, =MSG_NOVA_SENHA			;Ponteiro para a mensagem "  Nova senha:   "
+	BL		LCD_Write_String			;Função de escrever uma string no LCD, presta atenção no cursor(deixei ele piscando)
+	MOV		R0, #0x80					;Posição do inicio da primeira linha do cursor
+	BL		LCD_Move_Cursor
+	MOV		R0, '*'
+	BL		LCD_Write_Character			;Escreve um caracter na posição R0 do LCD
+	;BL		LCD_Reset;					;Limpa a tela do LCD e move o cursor para a primeira linha no inicio
+	
+; -------------------------------------------------------------------------------
+Main
+	
+;--------------------------------------------------------------------------------
+Fim
+	NOP;
+	
+; -------------------------------------------------------------------------------------------------------------------------
+; Fim do Arquivo
+; -------------------------------------------------------------------------------------------------------------------------	
+    ALIGN                        ;Garante que o fim da seção está alinhada 
+    END                          ;Fim do arquivo
